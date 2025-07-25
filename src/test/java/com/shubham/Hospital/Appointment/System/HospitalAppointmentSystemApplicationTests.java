2ae8d866559450b6aafea9698ab1406a67cd27f4
Shubham <shubhamkharche2003@gmail.com>
2025-07-25 10:37:07 +0530

    Hospital Appointment System project setup completed

---
+++ src/test/java/com/shubham/Hospital/Appointment/System/HospitalAppointmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shubham.Hospital.Appointment.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalAppointmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
