545300ceedd4ac9148764be4712e3fec9a01a381
Shubham <shubhamkharche2003@gmail.com>
2025-07-25 15:03:24 +0530

    GlobalExceptionHandler folder created and handle are the required exception

---
+++ src/main/java/com/shubham/Hospital/Appointment/System/Advice/GlobalExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.shubham.Hospital.Appointment.System.Advice;
+
+import com.shubham.Hospital.Appointment.System.Exception.AppointmentNotFoundException;
+import com.shubham.Hospital.Appointment.System.Exception.DoctorNotFoundException;
+import com.shubham.Hospital.Appointment.System.Exception.PatientNotFoundException;
+import com.shubham.Hospital.Appointment.System.Exception.PrescriptionNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(PatientNotFoundException.class)
+    public ResponseEntity<String> handleException(PatientNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NO_CONTENT);
+    }
+
+    @ExceptionHandler(DoctorNotFoundException.class)
+    public ResponseEntity<String> handleException(DoctorNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NO_CONTENT);
+    }
+
+    @ExceptionHandler(AppointmentNotFoundException.class)
+    public ResponseEntity<String> handleException(AppointmentNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NO_CONTENT);
+    }
+
+    @ExceptionHandler(PrescriptionNotFoundException.class)
+    public ResponseEntity<String> handleException(PrescriptionNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NO_CONTENT);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception ex) {
+        return new ResponseEntity<>("Internal Server Error!", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
