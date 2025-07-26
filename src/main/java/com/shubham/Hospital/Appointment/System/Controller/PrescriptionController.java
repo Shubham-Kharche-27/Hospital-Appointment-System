a0a64f9ed3ce0c19b07f7b406c986403e6e1bb22
Shubham <shubhamkharche2003@gmail.com>
2025-07-26 14:42:27 +0530

    PrescriptionService and PrescriptionController code completed

--- src/main/java/com/shubham/Hospital/Appointment/System/Controller/PrescriptionController.java
+++ src/main/java/com/shubham/Hospital/Appointment/System/Controller/PrescriptionController.java
@@ -1,4 +1,46 @@
 package com.shubham.Hospital.Appointment.System.Controller;
 
+import com.shubham.Hospital.Appointment.System.Dto.PrescriptionDto;
+import com.shubham.Hospital.Appointment.System.Service.PrescriptionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/prescription")
 public class PrescriptionController {
+
+    @Autowired
+    private PrescriptionService prescriptionService;
+
+    @GetMapping("/get")
+    public ResponseEntity<Page<PrescriptionDto>> getAllPrescriptionData(
+            @RequestParam(value = "pageNum",defaultValue = "1")int pageNum,
+            @RequestParam(value = "pageSize",defaultValue = "3")int pageSize,
+            @RequestParam(defaultValue = "prescriptionId")String sortBy
+    ){
+        return new ResponseEntity<>(prescriptionService.getAllPrescriptionData(pageNum-1,pageSize,sortBy), HttpStatus.OK);
+    }
+
+    @GetMapping("/get/byId/{prescriptionId}")
+    public ResponseEntity<PrescriptionDto> getPrescriptionById(@PathVariable Long prescriptionId){
+        return new ResponseEntity<>(prescriptionService.getPrescriptionDataById(prescriptionId),HttpStatus.OK);
+    }
+
+    @PostMapping("/post")
+    public ResponseEntity<String> createPrescription(@RequestBody PrescriptionDto prescriptionDto){
+        return new ResponseEntity<>(prescriptionService.createPrescription(prescriptionDto),HttpStatus.CREATED);
+    }
+
+    @PutMapping("/put/{prescriptionId}")
+    public ResponseEntity<String> updatePrescription(@PathVariable Long prescriptionId,@RequestBody PrescriptionDto prescriptionDto){
+        return new ResponseEntity<>(prescriptionService.updatePrescriptionData(prescriptionId,prescriptionDto),HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{prescriptionId}")
+    public ResponseEntity<String> deletePrescription(@PathVariable Long prescriptionId){
+        return new ResponseEntity<>(prescriptionService.deletePrescriptionData(prescriptionId),HttpStatus.NO_CONTENT);
+    }
 }
--- src/main/java/com/shubham/Hospital/Appointment/System/Service/PrescriptionService.java
+++ src/main/java/com/shubham/Hospital/Appointment/System/Service/PrescriptionService.java
@@ -1,4 +1,76 @@
 package com.shubham.Hospital.Appointment.System.Service;
 
+import com.shubham.Hospital.Appointment.System.Dto.PrescriptionDto;
+import com.shubham.Hospital.Appointment.System.Entity.Appointment;
+import com.shubham.Hospital.Appointment.System.Entity.Prescription;
+import com.shubham.Hospital.Appointment.System.Exception.AppointmentNotFoundException;
+import com.shubham.Hospital.Appointment.System.Exception.PrescriptionNotFoundException;
+import com.shubham.Hospital.Appointment.System.Repository.AppointmentRepo;
+import com.shubham.Hospital.Appointment.System.Repository.PrescriptionRepo;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PrescriptionService {
+
+    @Autowired
+    private PrescriptionRepo prescriptionRepo;
+
+    @Autowired
+    private AppointmentRepo appointmentRepo;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public Page<PrescriptionDto> getAllPrescriptionData(int pageNum, int pageSize, String sortBy) {
+        Pageable pageable = PageRequest.of(pageNum, pageSize, Sort.by(sortBy));
+        Page<Prescription> prescriptionPage = prescriptionRepo.findAll(pageable);
+        return prescriptionPage.map(Prescription -> modelMapper.map(Prescription, PrescriptionDto.class));
+    }
+
+    public PrescriptionDto getPrescriptionDataById(Long prescriptionId) {
+        Prescription prescription = prescriptionRepo.findById(prescriptionId)
+                .orElseThrow(() -> new PrescriptionNotFoundException("Prescription does not exist!"));
+        return modelMapper.map(prescription, PrescriptionDto.class);
+    }
+
+    public String createPrescription(PrescriptionDto prescriptionDto) {
+        if (prescriptionDto == null) {
+            throw new PrescriptionNotFoundException("Prescription data is empty!");
+        }
+        Prescription prescription = modelMapper.map(prescriptionDto, Prescription.class);
+        Appointment appointment = appointmentRepo.findById(prescriptionDto.getAppointmentId())
+                .orElseThrow(() -> new AppointmentNotFoundException("Appointment does not exist!"));
+        prescription.setAppointment(appointment);
+        prescriptionRepo.save(prescription);
+        return "Prescription created successfully!";
+    }
+
+    public String updatePrescriptionData(Long prescriptionId, PrescriptionDto prescriptionDto) {
+        Prescription prescription = prescriptionRepo.findById(prescriptionId)
+                .orElseThrow(() -> new PrescriptionNotFoundException("Prescription does mot exist!"));
+        if(prescriptionDto.getMedication()!=null){
+            prescription.setMedication(prescriptionDto.getMedication());
+        }
+        if(prescriptionDto.getDiagnosis()!=null){
+            prescription.setDiagnosis(prescriptionDto.getDiagnosis());
+        }
+        if(prescriptionDto.getAdvice()!=null){
+            prescription.setAdvice(prescriptionDto.getAdvice());
+        }
+        prescriptionRepo.save(prescription);
+        return "Prescription data updated successfully!";
+    }
+
+    public String deletePrescriptionData(Long prescriptionId){
+        Prescription prescription = prescriptionRepo.findById(prescriptionId)
+                .orElseThrow(()->new PrescriptionNotFoundException("Prescription does not exist!"));
+        prescriptionRepo.deleteById(prescriptionId);
+        return "Prescription deleted successfully!";
+    }
 }
