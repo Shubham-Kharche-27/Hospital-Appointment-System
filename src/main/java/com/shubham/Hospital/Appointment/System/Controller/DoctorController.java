4f712368b3f577461085a5b4ea52dfb419957238
Shubham <shubhamkharche2003@gmail.com>
2025-07-26 10:41:48 +0530

    DoctorService and DoctorController code was completed

--- src/main/java/com/shubham/Hospital/Appointment/System/Controller/DoctorController.java
+++ src/main/java/com/shubham/Hospital/Appointment/System/Controller/DoctorController.java
@@ -1,4 +1,46 @@
 package com.shubham.Hospital.Appointment.System.Controller;
 
+import com.shubham.Hospital.Appointment.System.Dto.DoctorDto;
+import com.shubham.Hospital.Appointment.System.Service.DoctorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/doctor")
 public class DoctorController {
+
+    @Autowired
+    private DoctorService doctorService;
+
+    @GetMapping("/get")
+    public ResponseEntity<Page<DoctorDto>> getAllDoctor(
+            @RequestParam(value = "pageNum",defaultValue = "1")int pageNum,
+            @RequestParam(value = "pageSize",defaultValue = "3")int pageSize,
+            @RequestParam(defaultValue = "doctorId")String sortBy
+    ){
+        return new ResponseEntity<>(doctorService.getAllDoctorData(pageNum-1,pageSize,sortBy), HttpStatus.OK);
+    }
+
+    @GetMapping("/get/byId/{doctorId}")
+    public ResponseEntity<DoctorDto> getDoctorById(@PathVariable Long doctorId){
+        return new ResponseEntity<>(doctorService.getDoctorDataById(doctorId),HttpStatus.OK);
+    }
+
+    @PostMapping("/post")
+    public ResponseEntity<String> createDoctor(@RequestBody DoctorDto doctorDto){
+        return new ResponseEntity<>(doctorService.createDoctorData(doctorDto),HttpStatus.CREATED);
+    }
+
+    @PutMapping("/put/{doctorId}")
+    public ResponseEntity<String> updateDoctor(@PathVariable Long doctorId,@RequestBody DoctorDto doctorDto){
+        return new ResponseEntity<>(doctorService.updateDoctorData(doctorId,doctorDto),HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/delete/{doctorId}")
+    public ResponseEntity<String> deleteDoctor(@PathVariable Long doctorId){
+        return new ResponseEntity<>(doctorService.deleteDoctorData(doctorId),HttpStatus.NO_CONTENT);
+    }
 }
--- src/main/java/com/shubham/Hospital/Appointment/System/Service/DoctorService.java
+++ src/main/java/com/shubham/Hospital/Appointment/System/Service/DoctorService.java
@@ -1,7 +1,80 @@
 package com.shubham.Hospital.Appointment.System.Service;
 
+import com.shubham.Hospital.Appointment.System.Dto.DoctorDto;
+import com.shubham.Hospital.Appointment.System.Entity.Doctor;
+import com.shubham.Hospital.Appointment.System.Exception.DoctorNotFoundException;
+import com.shubham.Hospital.Appointment.System.Repository.DoctorRepo;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DoctorService {
+
+    @Autowired
+    private DoctorRepo doctorRepo;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public Page<DoctorDto> getAllDoctorData(int pageNum,int pageSize,String sortBy){
+        Pageable pageable = PageRequest.of(pageNum,pageSize, Sort.by(sortBy));
+        Page<Doctor> doctorPage = doctorRepo.findAll(pageable);
+        Page<DoctorDto> doctorDtos = doctorPage.map(Doctor->modelMapper.map(Doctor,DoctorDto.class));
+        return doctorDtos;
+    }
+
+    public DoctorDto getDoctorDataById(Long doctorId){
+        Doctor doctor = doctorRepo.findById(doctorId)
+                .orElseThrow(()->new DoctorNotFoundException("Doctor does not exist!"));
+        return modelMapper.map(doctor, DoctorDto.class);
+    }
+
+    public String createDoctorData(DoctorDto doctorDto){
+        if(doctorDto==null){
+            throw new DoctorNotFoundException("Doctor data is empty!");
+        }
+        Doctor doctor = modelMapper.map(doctorDto,Doctor.class);
+        doctorRepo.save(doctor);
+        return "Doctor data created successfully!";
+    }
+
+    public String updateDoctorData(Long doctorId,DoctorDto doctorDto){
+        Doctor doctor = doctorRepo.findById(doctorId)
+                .orElseThrow(()->new DoctorNotFoundException("Doctor does not exist!"));
+        if(doctorDto.getDoctorName()!=null){
+            doctor.setDoctorName(doctorDto.getDoctorName());
+        }
+        if(doctorDto.getDoctorGender()!=null){
+            doctor.setDoctorGender(doctorDto.getDoctorGender());
+        }
+        if(doctorDto.getDoctorSpecialization()!=null){
+            doctor.setDoctorSpecialization(doctorDto.getDoctorSpecialization());
+        }
+        if(doctorDto.getDoctorEmail()!=null){
+            doctor.setDoctorEmail(doctorDto.getDoctorEmail());
+        }
+        if(doctorDto.getDoctorPhoneNum()!=null){
+            doctor.setDoctorPhoneNum(doctorDto.getDoctorPhoneNum());
+        }
+        if(doctorDto.getDoctorExperience()!=null){
+            doctor.setDoctorExperience(doctorDto.getDoctorExperience());
+        }
+        if(doctorDto.getDoctorDob()!=null){
+            doctor.setDoctorDob(doctorDto.getDoctorDob());
+        }
+        doctorRepo.save(doctor);
+        return "Doctor data updated successfully!";
+    }
+
+    public String deleteDoctorData(Long doctorId){
+        Doctor doctor = doctorRepo.findById(doctorId)
+                .orElseThrow(()->new DoctorNotFoundException("Doctor does not exist!"));
+        doctorRepo.deleteById(doctorId);
+        return "Doctor data deleted successfully!";
+    }
 }
